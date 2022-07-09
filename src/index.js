ffa58045759952ba776efeb829669c44b44567f6
Nevil Bhimani <nevilbhimani@Nevils-MacBook-Pro.local>
2022-07-09 11:57:11 +0530

    counter state with redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import allReducer from "./reducers";
+import { Provider } from "react-redux";
+// //store -> globalized state
+// //action increment
+// const increment = () => {
+//   return {
+//     type: "INCREMENT", //type or name
+//   };
+// };
+// const decrement = () => {
+//   return {
+//     type: "DECREMENT", //type or name
+//   };
+// };
+// //reducer -> how actions transform state into the next state
+// const counter = (state = 0, action) => {
+//   switch (action.type) {
+//     case "INCREMENT":
+//       return state + 1;
+//     case "DECREMENT":
+//       return state - 1;
+//   }
+// };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// let store = createStore(counter);
+// //display it in the console
+// store.subscribe(() => console.log(store.getState()));
+// //dispatch ->
+// store.dispatch(increment());
+
+const store = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
